54ec52210e679ab112cf50f08184e87b4eae17b4
Monir Ahmad <87666860+MonirAhmadJoy@users.noreply.github.com>
2022-05-31 17:44:28 +0600

    sdg
    jhtsn

---
+++ A.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+
+typedef long long ll;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+typedef vector<pll> vll;
+typedef tree< ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update > ordered_set;
+
+#define PB push_back
+#define F first
+#define S second
+#define endl '\n'
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b)))
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#define MOD 1000000007
+
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+
+/**
+inline void normal(ll &a) { a %= MOD; (a < 0) && (a += MOD); }
+inline ll modMul(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a*b)%MOD; }
+inline ll modAdd(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); return (a+b)%MOD; }
+inline ll modSub(ll a, ll b) { a %= MOD, b %= MOD; normal(a), normal(b); a -= b; normal(a); return a; }
+inline ll modPow(ll b, ll p) { ll r = 1; while(p) { if(p&1) r = modMul(r, b); b = modMul(b, b); p >>= 1; } return r; }
+inline ll modInverse(ll a) { return modPow(a, MOD-2); }
+inline ll modDiv(ll a, ll b) { return modMul(a, modInverse(b)); }
+*/
+int dx[]= {+1,-1,0,0,+1,+1,-1,-1};
+int dy[]= {0,0,+1,-1,+1,-1,+1,-1};
+///____________________________________________________________________________________________________________________///
+
+
+int main()
+{
+    optimize();
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n;
+        cin>>n;
+
+        ll a[n+3];
+        vl v;
+
+        for(ll i=0; i<n; i++)
+        {
+            cin>>a[i];
+            v.PB(a[i]);
+        }
+        ll ans;
+        sort(v.begin(),v.end());
+        for(ll i=0; i<n; i++)
+        {
+            if(a[i]!=v[i])
+            {
+                ans=a[i];
+                for(ll j=i+1;j<n;j++){
+                    if(a[j]<ans)
+                        ans=a[j];
+                }
+                break;
+            }
+        }
+        cout<<ans<<endl;
+    }
+
+
+    return 0;
+}
+
